1549a5dea643098e921b02b661f1085d527ac342
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2019-07-08 12:07:39 +0100

    Added routes for fep

--- app/routes.js
+++ app/routes.js
@@ -8,9 +8,19 @@ router.use((req, res, next) => {
   next()
 })
 
+router.use((req, res, next) => {
+  res.locals.back = req.headers.referer
+  next()
+})
+
 // Child death prototypes
 router.use(/\/prototypes\/child-death-journey\/version-([0-9]+)/, (req, res, next) => {
   return require(`./views/_prototypes/child-death-journey/version-${req.params[0]}/routes`)(req, res, next);
+})
+
+// FEP prototypes
+router.use(/\/prototypes\/fep\/version-([0-9]+)/, (req, res, next) => {
+  return require(`./views/_prototypes/fep/version-${req.params[0]}/routes`)(req, res, next);
 })
 
 module.exports = router
