2f1465be93938e0ca845605081e05fc4eddaf109
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2019-07-09 14:46:05 +0100

    Updated for scenarios

--- app/views/_prototypes/child-death-journey/version-1/routes.js
+++ app/views/_prototypes/child-death-journey/version-1/routes.js
@@ -39,7 +39,7 @@ router.get('/about-the-deceased-child', (req, res) => {
   res.render('_pages/death-notification/about-the-deceased-child/version-1')
 })
 router.post('/about-the-deceased-child', (req, res) => {
-  if (req.body.deceased.benefits && req.body.deceased.benefits.includes('Disability Living Allowance Child')) {
+  if (req.body.deceased.benefits && req.body.deceased.benefits.includes('Child Disability Living Allowance')) {
     return res.redirect('is-there-a-carer')
   }
   res.redirect('check-your-answers')
@@ -49,7 +49,7 @@ router.get('/about-the-deceased-16-19', (req, res) => {
   res.render('_pages/death-notification/about-the-deceased-16-19/version-1')
 })
 router.post('/about-the-deceased-16-19', (req, res) => {
-  if (req.body.deceased.benefits && req.body.deceased.benefits.includes('Disability Living Allowance Child')) {
+  if (req.body.deceased.benefits && req.body.deceased.benefits.includes('Child Disability Living Allowance')) {
     res.redirect('is-there-a-carer')
   } else {
     res.redirect('check-your-answers')
@@ -115,7 +115,7 @@ router.get('/fill-dummy-data', (req, res) => {
       },
       "nino": "aa123456a",
       "benefits": [
-        "Disability Living Allowance Child"
+        "Child Disability Living Allowance"
       ],
       "dod": {
         "day": "2",
